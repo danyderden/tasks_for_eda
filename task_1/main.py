81c8e35878125eeb6fe403d9fb0c4f6cc43dff5a
danyderden <danyderden@gmail.com>
2023-07-02 18:06:16 +0300

    total_population to __init__

--- task_1/main.py
+++ task_1/main.py
@@ -4,19 +4,20 @@ import random
 
 class RandomCity:
 
-    def __init__(self, route):
-        self.route: str = route
+    def __init__(self, file_path) -> None:
+        self.file_path: str = file_path
         self.cities: dict = self._load_cities()
+        self.total_population = sum(c['population'] for c in self.cities)
+
 
     def _load_cities(self) -> [dict]:
-        with open(self.route, 'r') as file:
+        with open(self.file_path, 'r') as file:
             cities = json.load(file)
         return cities
 
     def get_random_city(self) -> str:
-        total_population = sum(c['population'] for c in self.cities)
         random_probability = random.random()
         for city in self.cities:
-            if city['population'] / total_population >= random_probability:
+            if city['population'] / self.total_population >= random_probability:
                 return city['name']
         return self.get_random_city()
